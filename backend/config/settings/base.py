bd623af3efb572b82b9847fb3288a67650374c5d
DongwookKim0823 <dwkim0823@naver.com>
2024-01-13 14:54:57 +0900

    chore: 개발 환경 분리
    - settings.py 디렉토리 이동 후 base.py로 이름 변경
    - base.py의 BASE_DIR 수정
    - base.py에서 SECRET_KEY, DEBUG, ALLOWED_HOSTS, DATABASES 설정 삭제
    - dev.py를 환경 변수 로딩 방식으로 작성
    - 환경 변수 관리를 위해 django-environ 패키지 추가
    - .gitignore에 환경 변수 파일 추가
    - manage.py와 wsgi.py에서 설정 변경

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -13,19 +13,11 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@rm@a=9zw#_acy*6+*9c8!ahrtplkob99p_-vsc*_$(pzo+vls'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -68,17 +60,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
---
+++ backend/config/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+# 실제 운영 환경과 관련된 설정 추가
+# SECRET_KEY 생성, ALLOWED_HOSTS에 운영환경 ip 등록, 데이터베이스 설정, 로깅 설정 등 추가 예정
+
+# SECRET_KEY =
+#
+# DEBUG = False
+#
+# ALLOWED_HOSTS = []
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
