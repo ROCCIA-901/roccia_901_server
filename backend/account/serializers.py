5464ca81f68a9adb3215d9f2488248188b4e07dc
DongwookKim0823 <dwkim0823@naver.com>
2024-01-20 23:03:51 +0900

    fix: 회원가입 기능 수정
    - 모델 수정(슈퍼유저 생성 메서드 추가, CHOICES 필드값 변경, User 모델에 str 메서드 추가)
    - 마이그레이션 파일 재생성
    - UserRegistrationSerializer에 검증 로직 추가
    - UserRegisterAPIView에 permission_classes로 AllowAny 추가

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,42 +1,57 @@
-from django.contrib.auth import get_user_model
+import re
+
 from rest_framework import serializers
+from .models import User
 
-User = get_user_model()
 
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password_confirmation = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
-class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            password=validated_data['password'],
-            username=validated_data['username'],
-            generation=validated_data['generation'],
-            role=validated_data['role'],
-            workout_location=validated_data['workout_location'],
-            workout_level=validated_data['workout_level'],
-            profile_number=validated_data['profile_number'],
+        fields = (
+            'email', 'password', 'password_confirmation', 'username', 'generation', 'role',
+            'workout_location', 'workout_level', 'profile_number', 'introduction'
         )
-        return user
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("이미 존재하는 이메일입니다.")
+        return value
+
+    def validate_username(self, value):
+        pattern = r'^[가-힣]{2,4}$'  # 끝을 나타내는 $ 기호를 추가
+        if not re.match(pattern, value):
+            raise serializers.ValidationError("이름이 정확하지 않습니다.")
+        return value
 
     def validate(self, data):
-        required_fields = [
-            'email',
-            'password',
-            'username',
-            'generation',
-            'role',
-            'workout_location',
-            'workout_level',
-            'profile_number',
-        ]
+        password = data.get('password')
+        password_confirmation = data.get('password_confirmation')
 
-        for field in required_fields:
-            if field not in data or not data[field]:
-                raise serializers.ValidationError(f"{field} 필드는 비어 있을 수 없습니다.")
+        if password != password_confirmation:
+            raise serializers.ValidationError({"password_confirmation": "비밀번호가 일치하지 않습니다."})
+
+        if len(password) < 7:
+            raise serializers.ValidationError({"password": "비밀번호는 최소 7자 이상이어야 합니다."})
+
+        if not any(char.isupper() for char in password):
+            raise serializers.ValidationError({"password": "비밀번호에는 최소 1개 이상의 대문자가 포함되어야 합니다."})
+
+        if not any(char.islower() for char in password):
+            raise serializers.ValidationError({"password": "비밀번호에는 최소 1개 이상의 소문자가 포함되어야 합니다."})
+
+        if not any(char.isdigit() for char in password):
+            raise serializers.ValidationError({"password": "비밀번호에는 최소 1개 이상의 숫자가 포함되어야 합니다."})
+
+        special_characters = "[~!@#$%^&*()_+{}\":;'<>?,./]"
+        if not any(char in special_characters for char in password):
+            raise serializers.ValidationError({"password": "비밀번호에는 최소 1개 이상의 특수 문자가 포함되어야 합니다."})
 
         return data
 
+    def create(self, validated_data):
+        validated_data.pop('password_confirmation', None)
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/account/views.py
+++ backend/account/views.py
@@ -3,27 +3,30 @@ from .serializers import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 
-class RegisterAPIView(APIView):
+class UserRegistrationAPIView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid()
+        user = serializer.save()
 
-            token = TokenObtainPairSerializer.get_token(user)
-            refresh_token = str(token)
-            access_token = str(token.access_token)
-            return Response(
-                {
-                    "message": "회원가입에 성공했습니다.",
-                    "data": {
-                        "user": serializer.data,
-                        "token": {
-                            "access": access_token,
-                            "refresh": refresh_token,
-                        },
-                    }
-                },
-                status=status.HTTP_200_OK,
-            )
+        token = TokenObtainPairSerializer.get_token(user)
+        refresh_token = str(token)
+        access_token = str(token.access_token)
+        return Response(
+            {
+                "message": "회원가입에 성공했습니다.",
+                "data": {
+                    "user": serializer.data,
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                }
+            },
+            status=status.HTTP_200_OK,
+        )
