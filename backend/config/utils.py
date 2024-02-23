de85757b205a5c4aca076b74a0ee679204f36686
DongwookKim0823 <dwkim0823@naver.com>
2024-02-24 02:29:37 +0900

    fix: 커스텀 핸들러 수정
    - 커스텀 핸들러에서 토큰 관련 에러 처리 하도록 수정

--- backend/config/utils.py
+++ backend/config/utils.py
@@ -1,8 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import exception_handler
+from rest_framework_simplejwt.authentication import (
+    AuthenticationFailed,
+    InvalidToken,
+    TokenError,
+)
 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
+
+    if isinstance(exc, AuthenticationFailed):
+        return Response({"message": "인증에 실패했습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+    elif isinstance(exc, InvalidToken):
+        return Response({"message": "유효하지 않은 토큰입니다."}, status=status.HTTP_401_UNAUTHORIZED)
+    elif isinstance(exc, TokenError):
+        return Response({"message": "토큰 처리 중 오류가 발생했습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
     if response is not None and "message" not in response.data:
         error_messages = []
@@ -14,7 +28,5 @@ def custom_exception_handler(exc, context):
                 error_messages.append(messages)
 
         combined_message = " ".join(error_messages)
-
         response.data = {"message": combined_message}
-
     return response
