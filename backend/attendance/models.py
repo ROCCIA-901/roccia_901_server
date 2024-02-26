44f3928d3828f9feea24797ee76a92472dabecf5
DongwookKim0823 <dwkim0823@naver.com>
2024-02-26 16:51:16 +0900

    feat: 출석하기 기능 구현
    - 특정 필드 null 허용으로 수정
    - 마이그레이션 파일 생성
    - 출석 요청 기능 구현(값 검증 기능은 추후에 개발)
    
    Resolved #16

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -23,17 +23,17 @@ class Attendance(models.Model):
 
     user_id: models.IntegerField = models.IntegerField(help_text="사용자 ID")
     workout_location: models.CharField = models.CharField(
-        max_length=100, choices=WORKOUT_LOCATION_CHOICES, help_text="운동 지점"
+        max_length=100, choices=WORKOUT_LOCATION_CHOICES, null=True, help_text="운동 지점"
     )
-    week: models.IntegerField = models.IntegerField(help_text="주차")
+    week: models.IntegerField = models.IntegerField(null=True, help_text="주차")
     request_time: models.DateTimeField = models.DateTimeField(help_text="출석 요청 시간")
     request_processed_status: models.CharField = models.CharField(
-        max_length=20, choices=REQUEST_STATUS_CHOICES, help_text="요청 상태"
+        max_length=20, choices=REQUEST_STATUS_CHOICES, null=True, help_text="요청 상태"
     )
-    request_processed_time: models.DateTimeField = models.DateTimeField(help_text="요청 처리 시간")
-    request_processed_user_id: models.IntegerField = models.IntegerField(help_text="요청을 처리한 사용자 ID")
+    request_processed_time: models.DateTimeField = models.DateTimeField(null=True, help_text="요청 처리 시간")
+    request_processed_user_id: models.IntegerField = models.IntegerField(null=True, help_text="요청을 처리한 사용자 ID")
     attendance_status: models.CharField = models.CharField(
-        max_length=20, choices=ATTENDANCE_STATUS_CHOICES, help_text="출석 상태"
+        max_length=20, choices=ATTENDANCE_STATUS_CHOICES, null=True, help_text="출석 상태"
     )
 
     class Meta:
--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -1,1 +1,7 @@
-# from django.urls import path
+from django.urls import path
+
+from attendance.views import AttendanceRequest
+
+urlpatterns = [
+    path("", AttendanceRequest.as_view(), name="attendance-request"),
+]
