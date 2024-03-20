ba77c5767fe6da3cc6d750a32aeacd7b8cf4fcc8
ikasoon <ikasoon119@gmail.com>
2024-03-20 14:24:08 +0900

    fix: Ranking model 하나로 통합

--- backend/ranking/models.py
+++ backend/ranking/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from account.models import User
 
 
-class WeeklyRanking(models.Model):
+class Ranking(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="weekly_ranking", help_text="사용자 ID"
+        User, on_delete=models.CASCADE, related_name="ranking", help_text="사용자 ID"
     )  # type: ignore
-    week = models.PositiveIntegerField()  # type: ignore
-    rank = models.PositiveIntegerField()  # type: ignore
-    score = models.FloatField(default=0.0)  # type: ignore
+    generation = models.PositiveIntegerField(help_text="운동 기간")  # type: ignore
+    week = models.PositiveIntegerField(help_text="운동 주차")  # type: ignore
+    score = models.FloatField(default=0.0, help_text="운동 점수")  # type: ignore
 
     class Meta:
-        db_table = "weekly_ranking"
+        db_table = "ranking"
         ordering = ["week"]
