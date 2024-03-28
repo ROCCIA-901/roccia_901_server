a11971b3e454943c2943020bc27f415b58a1883b
ikasoon <ikasoon119@gmail.com>
2024-03-28 23:52:37 +0900

    feat: 랭킹 집계 job && runapscheduler command 추가

---
+++ backend/ranking/management/commands/runapscheduler.py
@@ -0,0 +1,45 @@
+import logging
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django_apscheduler import util
+from django_apscheduler.jobstores import DjangoJobStore
+
+from ranking.jobs import compile_rankings
+
+logger = logging.getLogger(__name__)
+
+
+@util.close_old_connections
+def compile_rankings_job():
+    (year, week, _) = compile_rankings()
+    logger.info(f"Rankings for the {week}th week of {year} completed.")
+
+
+class Command(BaseCommand):
+    help = "Runs APScheduler."
+
+    def handle(self, *args, **options):
+        scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+
+        scheduler.add_job(
+            compile_rankings_job,
+            trigger=CronTrigger(
+                day_of_week="mon", hour="00", minute="00"
+            ),  # Midnight on Monday, before start of the next work week.
+            id="compile_rankings",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("Added weekly job: 'compile_rankings_job'.")
+
+        try:
+            logger.info("Starting scheduler...")
+            scheduler.start()
+        except KeyboardInterrupt:
+            logger.info("Stopping scheduler...")
+            scheduler.shutdown()
+            logger.info("Scheduler shut down successfully!")
