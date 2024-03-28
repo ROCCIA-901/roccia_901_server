9cfa25caa72b847422d71200d73d55c76ddb6e9f
ikasoon <ikasoon119@gmail.com>
2024-03-28 22:38:43 +0900

    feat: Ranking model에 create_at field 추가

--- backend/ranking/models.py
+++ backend/ranking/models.py
@@ -12,6 +12,7 @@ class Ranking(models.Model):
     generation = models.PositiveIntegerField(help_text="운동 기간")  # type: ignore
     week = models.PositiveIntegerField(help_text="운동 주차")  # type: ignore
     score = models.FloatField(default=0.0, help_text="운동 점수")  # type: ignore
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성 시각")  # type: ignore
 
     class Meta:
         db_table = "ranking"
