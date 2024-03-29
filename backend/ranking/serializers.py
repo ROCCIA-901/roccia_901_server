3ea6d8865056dc610ce898b0ac2c8762eb2e0bfb
soonvro <soonvro@gmail.com>
2024-03-29 15:32:46 +0900

    refactor: queryset에 포함된 field를 가져오게 serializer 수정

--- backend/ranking/serializers.py
+++ backend/ranking/serializers.py
@@ -5,16 +5,18 @@ from ranking.models import Ranking
 
 
 class RankingSerializer(serializers.ModelSerializer):
-    username = ReadOnlyField(source="user.username")
-    user_generation = ReadOnlyField(source="user.generation")
-    user_workout_location = ReadOnlyField(source="user.workout_location")
-    user_workout_level = ReadOnlyField(source="user.workout_level")
-    user_profile_number = ReadOnlyField(source="user.profile_number")
+    user_id = ReadOnlyField(source="user__id")
+    username = ReadOnlyField(source="user__username")
+    user_generation = ReadOnlyField(source="user__generation")
+    user_workout_location = ReadOnlyField(source="user__workout_location")
+    user_workout_level = ReadOnlyField(source="user__workout_level")
+    user_profile_number = ReadOnlyField(source="user__profile_number")
 
     class Meta:
         model: type[Ranking] = Ranking
         fields: tuple = (
             "score",
+            "user_id",
             "username",
             "user_generation",
             "user_workout_location",
