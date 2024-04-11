a26f96c018cfd328a9baaabee4a3658725193ff9
DongwookKim0823 <74606041+DongwookKim0823@users.noreply.github.com>
2024-04-12 01:21:29 +0900

    chore: init.sh 수정
    - runapscheduler 실행 명령어 수정

--- backend/init.sh
+++ backend/init.sh
@@ -1,5 +1,5 @@
 python manage.py migrate --no-input
 python manage.py initadmin
 python manage.py collectstatic --no-input
-python manage.py runapscheduler.py
+python manage.py runapscheduler
 gunicorn --config gunicorn.conf.py config.wsgi:application
