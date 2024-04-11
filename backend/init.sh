aef163213b17f0f9226bb500e61df742e997e354
DongwookKim0823 <dwkim0823@naver.com>
2024-04-11 22:39:59 +0900

    chore: init.sh 수정
    - 스케줄러 실행 명령어 추가

--- backend/init.sh
+++ backend/init.sh
@@ -1,4 +1,5 @@
 python manage.py migrate --no-input
 python manage.py initadmin
 python manage.py collectstatic --no-input
+python manage.py runapscheduler.py
 gunicorn --config gunicorn.conf.py config.wsgi:application
