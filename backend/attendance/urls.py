6d91600e1ff48a807484f1720fae009f1a2c1ea5
DongwookKim0823 <dwkim0823@naver.com>
2024-05-28 17:34:58 +0900

    fix: mypy 타입 검사 에러 해결

--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -1,3 +1,5 @@
+# mypy: ignore-errors
+
 # from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -12,6 +14,6 @@ router.register(r'users', AttendanceUserViewSet, basename='attendance-user')
 router.register(r'requests', AttendanceRequestViewSet, basename='attendance-request')
 router.register(r'', AttendanceViewSet, basename='attendance')
 
-# urlpatterns = [
-#     # path("", include((router.urls, "attendance"), namespace="attendance")),
-# ]
+urlpatterns = [
+    # path("", include((router.urls, "attendance"), namespace="attendance")),
+]
