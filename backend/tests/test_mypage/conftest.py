9e057258a077dccdb73ffd44e5e8e887b421d8be
DongwookKim0823 <dwkim0823@naver.com>
2024-05-30 12:44:12 +0900

    test: MypageSerializer 단위 테스트 작성
    - 직렬화 테스트

--- backend/tests/test_mypage/conftest.py
+++ backend/tests/test_mypage/conftest.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from tests.test_account.factories import UserFactory
@@ -6,3 +8,15 @@ from tests.test_account.factories import UserFactory
 @pytest.fixture
 def mock_user_model():
     return UserFactory.build()
+
+
+@pytest.fixture
+def mock_boulder_problem_object():
+    with patch("record.models.BoulderProblem.objects.filter") as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_record_object():
+    with patch("record.models.Record.objects.filter") as mock:
+        yield mock
--- backend/tests/test_mypage/test_serializers.py
+++ backend/tests/test_mypage/test_serializers.py
@@ -1,5 +1,11 @@
+from unittest.mock import MagicMock
+
 from account.models import User
-from mypage.serializers import LevelCountSerializer, UserProfileSerializer
+from mypage.serializers import (
+    LevelCountSerializer,
+    MypageSerializer,
+    UserProfileSerializer,
+)
 
 
 class TestUserProfileSerializer:
@@ -25,3 +31,36 @@ class TestLevelCountSerializer:
         data = {"workout_level": 1, "total_count": 10}
         serializer = LevelCountSerializer(data)
         assert serializer.data == {"workout_level": "하얀색", "total_count": 10}
+
+
+class TestMypageSerializer:
+
+    def test_mypage_serializer(self, mock_boulder_problem_object, mock_record_object, mock_user_model):
+        mock_boulder_problem_object.return_value.values.return_value.annotate.return_value.order_by.return_value = [
+            {"workout_level": 1, "total_count": 10},
+            {"workout_level": 2, "total_count": 20},
+        ]
+        mock_record_object.return_value.annotate.return_value.aggregate.return_value = {
+            "total": MagicMock(total_seconds=MagicMock(return_value=330 * 60))
+        }
+        serializer = MypageSerializer(instance=mock_user_model)
+        data = serializer.data
+
+        expected_data = {
+            "profile": {
+                "username": mock_user_model.username,
+                "generation": mock_user_model.generation,
+                "role": mock_user_model.role,
+                "workout_location": mock_user_model.workout_location,
+                "workout_level": dict(User.WORKOUT_LEVELS)[mock_user_model.workout_level],
+                "profile_number": mock_user_model.profile_number,
+                "introduction": mock_user_model.introduction,
+            },
+            "total_workout_time": 330,
+            "records": [
+                {"workout_level": "하얀색", "total_count": 10},
+                {"workout_level": "노란색", "total_count": 20},
+            ],
+        }
+
+        assert data == expected_data
