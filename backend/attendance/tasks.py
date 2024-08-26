cdec5991e6e237d5c3c5bcd1a95b7e8a7d869ba1
DongwookKim0823 <dwkim0823@naver.com>
2024-08-26 16:30:39 +0900

    refactor: 대기 상태 출석 요청을 처리하는 테스크 수정
    - 테스크 수행 조건 추가

--- backend/attendance/tasks.py
+++ backend/attendance/tasks.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 from django.utils import timezone
 
+from account.models import Generation
 from attendance.models import Attendance
 from config.celery import app
 
@@ -11,9 +12,12 @@ logger = logging.getLogger("django")
 
 @app.task(name="reject_pending_attendances")
 def reject_pending_attendances():
+    """
+    처리되지 않은 '대기' 상태의 출석 요청을 거절 처리 하는 테스크입니다.
+    """
+
     current_date = timezone.now().date()
-
     yesterday = current_date - timedelta(days=1)
-    pending_attendances = Attendance.objects.filter(request_processed_status="대기", request_time__date=yesterday)
-
-    pending_attendances.update(request_processed_status="거절")
+    if Generation.objects.filter(start_date__lte=yesterday, end_date__gte=yesterday).exists():
+        pending_attendances = Attendance.objects.filter(request_processed_status="대기", request_time__date=yesterday)
+        pending_attendances.update(request_processed_status="거절")
